c2968002f67e8016d8d748ecd31eb066cd119b0e
Mandy Josse <mandyjosse@gmail.com>
2025-10-23 20:32:49 +0200

    test git pages2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        page_pproduit: resolve(__dirname, "pages/page_pproduit.html"),
+        page_produit: resolve(__dirname, "pages/page_produit.html"),
         compte: resolve(__dirname, "pages/compte.html"),
         panier: resolve(__dirname, "pages/panier.html"),
         validation_commande: resolve(__dirname, "pages/validation_commande.html"),
